0b78ae9b173cc0ead09ee9735c1dd5c4b1410275
Yasin Vəliyev <68180570+YasinVeliyev@users.noreply.github.com>
2022-05-06 23:10:37 +0400

    modified:   python/flatten.py

--- python/flatten.py
+++ python/flatten.py
@@ -3,11 +3,24 @@ from hashlib import new
 
 l = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
 
-def flatten(arr:list,newarr=[]) ->list:
+
+def flatten(arr: list, newarr=[]) -> list:
     for i in arr:
-        if type(i)==type([]):
-            newarr.extend(flatten(i,newarr=[]))
+        if type(i) == type([]):
+            newarr.extend(flatten(i, newarr=[]))
         else:
             newarr.append(i)
     return newarr
-  
+
+
+r = [[1, 2],[[5,6,7]], [3, 4], [5, 6, 7]]
+
+
+def reverse(arr):
+    arr.reverse()
+    for i in arr:
+        if type(i)==type([]):
+           reverse(i)
+    return arr
+
+print(reverse(r))
