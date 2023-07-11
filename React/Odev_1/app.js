5234409b04616267f06c85b62f67beba50316477
YasinVeliyev <veliyev.yasin@gmail.com>
2023-07-12 00:15:58 +0400

    modified:   React/Odev_1/app.js

--- React/Odev_1/app.js
+++ React/Odev_1/app.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function getData(id) {
-    let user = (await axios(`https://jsonplaceholder.typicode.com/users/${id}`))["data"];
+    let { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${id}`);
     user["posts"] = (await axios(`https://jsonplaceholder.typicode.com/posts?userId=${id}`))["data"];
     return user;
 }
