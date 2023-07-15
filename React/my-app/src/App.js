313f394492a1ef9e978ffafcf6e953a35731c085
YasinVeliyev <veliyev.yasin@gmail.com>
2023-07-16 01:01:09 +0400

    modified:   React/my-app/src/App.js 	modified:   React/my-app/src/Components/Header.js

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -15,6 +15,12 @@ class App extends Component {
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
                 <Header />
+                <Header />
+                <Header />
+                <Header />
+                <Header />
+                <Header />
+                <Header />
             </div>
         );
     }
