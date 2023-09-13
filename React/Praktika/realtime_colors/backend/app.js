368d600befe34525860d5eb83477d1a5bfbd7676
YasinVeliyev <veliyev.yasin@gmail.com>
2023-09-13 23:52:09 +0400

    modified:   .gitignore 	deleted:    React/Praktika/chat_app/src/App.css 	deleted:    React/Praktika/chat_app/src/App.js 	deleted:    React/Praktika/chat_app/src/index.js 	renamed:    React/Praktika/chat_app/backend/app.js -> React/Praktika/realtime_colors/backend/app.js 	renamed:    React/Praktika/chat_app/backend/package.json -> React/Praktika/realtime_colors/backend/package.json 	renamed:    React/Praktika/chat_app/backend/yarn.lock -> React/Praktika/realtime_colors/backend/yarn.lock 	renamed:    React/Praktika/chat_app/.gitignore -> React/Praktika/realtime_colors/client/.gitignore 	renamed:    React/Praktika/chat_app/README.md -> React/Praktika/realtime_colors/client/README.md 	renamed:    React/Praktika/chat_app/package-lock.json -> React/Praktika/realtime_colors/client/package-lock.json 	renamed:    React/Praktika/chat_app/package.json -> React/Praktika/realtime_colors/client/package.json 	renamed:    React/Praktika/chat_app/public/favicon.ico -> React/Praktika/realtime_colors/client/public/favicon.ico 	renamed:    React/Praktika/chat_app/public/index.html -> React/Praktika/realtime_colors/client/public/index.html 	new file:   React/Praktika/realtime_colors/client/src/App.css 	new file:   React/Praktika/realtime_colors/client/src/App.jsx 	renamed:    React/Praktika/chat_app/src/App.test.js -> React/Praktika/realtime_colors/client/src/App.test.js 	new file:   React/Praktika/realtime_colors/client/src/components/Pallet.jsx 	renamed:    React/Praktika/chat_app/src/index.css -> React/Praktika/realtime_colors/client/src/index.css 	new file:   React/Praktika/realtime_colors/client/src/index.js 	renamed:    React/Praktika/chat_app/src/logo.svg -> React/Praktika/realtime_colors/client/src/logo.svg 	new file:   React/Praktika/realtime_colors/client/src/socketApi.js 	new file:   React/Praktika/realtime_colors/client/yarn.lock

--- React/Praktika/realtime_colors/backend/app.js
+++ React/Praktika/realtime_colors/backend/app.js
@@ -1,31 +1,39 @@
 const app = require("express")();
-const http = require("http").createServer(app);
+const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const cors = require("cors");
+
+const Messages = require("./lib/Messages");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-	res.send("hello");
+	res.end("Merhaba Socket.IO");
 });
 
+<<<<<<<< HEAD:React/Praktika/realtime_colors/backend/app.js
 let lastColor = "#282c33";
 
+========
+>>>>>>>> ca350ea (	modified:   .gitignore):React/Praktika/chat_app/backend/app.js
 io.on("connection", (socket) => {
-	console.log("bir kullanıcı bağlandı!");
+	console.log("a user connected");
 
-	socket.emit("receive", lastColor);
-
-	socket.on("newColor", (color) => {
-		console.log(color);
-
-		lastColor = color;
-		io.emit("receive", color);
+	Messages.list((data) => {
+		console.log(data);
+		socket.emit("message-list", data);
 	});
 
-	socket.on("disconnect", () => {
-		console.log("Bir kullanıcı ayrıldı.");
+	socket.on("new-message", (message) => {
+		console.log(message);
+		Messages.upsert({ message });
+
+		socket.broadcast.emit("receive-message", message);
 	});
+
+	socket.on("disconnect", () => console.log("a user disconnected"));
 });
 
-http.listen(3001, () => console.log("Server is up 🚀 🚀"));
+http.listen(process.env.PORT || "3000", () => {
+	console.log("listening on *:3000");
+});
